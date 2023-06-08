369e76a38464ca33b279c583a93e4e2d2ae3573b
sanchda <838104+sanchda@users.noreply.github.com>
2023-06-08 06:40:01 -0500

    Add some psutil stuff

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM python:3.9.6-buster
 WORKDIR /app
+RUN pip install psutil
 COPY read_procfs.py .
 CMD ["python", "./read_procfs.py"]
--- read_procfs.py
+++ read_procfs.py
@@ -1,5 +1,35 @@
+import os
+import psutil
+
+def collect_runtime_metrics():
+  proc = psutil.Process(os.getpid())
+  stored_value = dict(
+      CTX_SWITCH_VOLUNTARY_TOTAL=0,
+      CTX_SWITCH_INVOLUNTARY_TOTAL=0,
+  )
+
+  with proc.oneshot():
+    try:
+      ctx_switch_voluntary_total = proc.num_ctx_switches().voluntary
+      ctx_switch_involuntary_total = proc.num_ctx_switches().involuntary
+    except:
+      ctx_switch_voluntary_total = ctx_switch_involuntary_total = 0
+
+      ctx_switch_voluntary = ctx_switch_voluntary_total - stored_value["CTX_SWITCH_VOLUNTARY_TOTAL"]
+      ctx_switch_involuntary = ctx_switch_involuntary_total - stored_value["CTX_SWITCH_INVOLUNTARY_TOTAL"]
+
+      metrics = [
+          ("CTX_SWITCH_VOLUNTARY", ctx_switch_voluntary),
+          ("CTX_SWITCH_INVOLUNTARY", ctx_switch_involuntary),
+      ]
+
+      return metrics
+
+
 try:
   with open('/proc/self/status') as f:
     print(f.read())
 except Exception as e:
   print(f"An error occurred: {e}")
+
+print(collect_runtime_metrics())
